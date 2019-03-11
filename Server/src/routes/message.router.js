344ac88b649ac568087a96889c00843d5455ee76
Chima <sebastinechima@gmail.com>
2019-03-11 09:22:04 +0100

    feat(endpoint): implement endpoint specific-message
    - ensure user model is created.
    - write tests for the methods
    - define endpoint and method for specific message retrieval
    
    [Delivers #164525798]

--- Server/src/routes/message.router.js
+++ Server/src/routes/message.router.js
@@ -7,7 +7,7 @@ router.post('/', messageControllers.createMessage);
 router.get('/', messageControllers.findAllReceivedMessages);
 router.get('/unread', messageControllers.findUnreadMessages);
 router.get('/sent', messageControllers.findSentMessages);
-// router.get('/:id', messageControllers.getOneMessage);
+router.get('/:id', messageControllers.getOneMessage);
 // router.delete('/:id', messageControllers.deleteMessage);
 
 export default router;
