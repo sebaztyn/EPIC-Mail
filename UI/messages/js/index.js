0760e68a5b5dd6d509ac3e454b5ddd5677fed0ff
Chima <sebastinechima@gmail.com>
2019-03-13 23:00:48 +0100

    chore(user): Redesign homepage UI
    - ensure a plain background is used
    - fix indentation to two spaces
    - define endpoint and method for user signup
    
    [Delivers #164618387]

--- UI/messages/js/index.js
+++ UI/messages/js/index.js
@@ -1,29 +1,54 @@
-let button = document.getElementById("toggle-menu"),
-  navContainer = document.getElementById("homepage-navbar"),
-  modal = document.getElementById("modal-test");
+const button = document.getElementById('toggle-menu');
+const navContainer = document.getElementById('homepage-navbar');
+const loginLink = document.getElementById('loginlink');
+const signupLink = document.getElementById('signuplink');
+const signupForm = document.getElementById('signup-form');
+const loginForm = document.getElementById('login-form');
+const modal = document.getElementById('modal-test');
+const loginDiv = document.getElementById('loginform-container');
+const signupDiv = document.getElementById('signupform-container');
+const passwordReset = document.getElementById('password-reset');
+const passwordForm = document.getElementById('password-form');
+const messageContainer = document.querySelector('.display-message');
+const sender = document.querySelector('sender');
+const subject = document.querySelector('subject');
+const mainBody = document.getElementsByTagName('body');
+const body = document.querySelector('message-body');
 
-let messageContainer = document.querySelector(".display-message"),
-  sender = document.querySelector("sender"),
-  subject = document.querySelector("subject"),
-  body = document.querySelector("message-body");
-
-button.addEventListener("click", function(event) {
+button.addEventListener('click', (event) => {
   event.preventDefault();
-  console.log("hello");
-  navContainer.classList.remove("toggle-off");
-  modal.style.display = "block";
-  navContainer.classList.add("toggle-on");
+  navContainer.classList.remove('toggle-off');
+  modal.style.display = 'block';
+  navContainer.classList.add('toggle-on');
+  mainBody[0].style.overflow = 'hidden';
 });
 
-window.addEventListener("mouseup", function(event) {
-  if (
-    event.target !== navContainer &&
-    event.target.parentNode !== navContainer
-  ) {
-    navContainer.classList.add("toggle-off");
-    navContainer.classList.remove("toggle-on");
-    modal.style.display = "none";
-
-    console.log("hey");
+window.addEventListener('mouseup', (event) => {
+  if (event.target !== navContainer && event.target.parentNode !== navContainer) {
+    navContainer.classList.add('toggle-off');
+    navContainer.classList.remove('toggle-on');
+    modal.style.display = 'none';
+    mainBody[0].style.overflow = 'auto';
   }
 });
+
+loginLink.addEventListener('click', () => {
+  loginForm.classList.remove('form-display');
+  signupForm.classList.add('form-display');
+  passwordForm.classList.add('form-display');
+});
+
+signupLink.addEventListener('click', () => {
+  loginForm.classList.add('form-display');
+  passwordForm.classList.add('form-display');
+  signupForm.classList.remove('form-display');
+});
+
+passwordReset.addEventListener('click', () => {
+  loginForm.classList.add('form-display');
+  signupForm.classList.add('form-display');
+  passwordForm.classList.remove('form-display');
+});
+
+
+
