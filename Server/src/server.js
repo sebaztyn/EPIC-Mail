201f75767f2187d7041bbe1a107e7bb447d5c9cb
Chima <sebastinechima@gmail.com>
2019-03-15 03:33:23 +0100

    chore(user): implement a UI redesign
    Add index.html file to the root of UI folder
    Rearrange the menu
    Separate login and sign up pages

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,17 +1,23 @@
 import express from 'express';
-import userRouters from './routes/user.router';
+import YAML from 'yamljs';
+import swaggerUi from 'swagger-ui-express';
 import messageRouters from './routes/message.router';
 import authenticationRouter from './routes/authenticationRouter';
 
+const swaggerDocument = YAML.load(`${__dirname}/../../swagger.yaml`);
+
 const app = express();
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(express.json());
+
 
 // app.use('/api/v1/users', userRouters);
 app.use('/api/v1/auth/', authenticationRouter);
 app.use('/api/v1/messages', messageRouters);
 
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 3000;
 
 const appServer = app.listen(port, console.log(`App running on port ${port}`));
 
